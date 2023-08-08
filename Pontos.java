9ce60e114bea628bd234b4c2ad08a8c09a2eaf10
Luis Henrique <128126715+souHenrique@users.noreply.github.com>
2023-08-07 21:44:59 -0300

    Add files via upload

---
+++ Pontos.java
@@ -0,0 +1,14 @@
+public class Pontos {
+    private double x;
+    private double y;
+    public Pontos(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    public double definirX() {
+        return x;
+    }
+    public double definirY() {
+        return y;
+    }
+}
